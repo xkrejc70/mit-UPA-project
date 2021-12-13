0ea5a23a114f0ccc46bb59b767c0934218e5bba4
Knight_of_Maids <matejkudera1@gmail.com>
2021-12-13 21:56:43 +0100

    Visualization: D1,D2, A2 added regions names

--- src/visualize_data.py
+++ src/visualize_data.py
@@ -6,14 +6,6 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from os import path
 
-def load_csv(path):
-    pass
-
-def store_graph(graph, name):
-    path = os.path.join(utils.graphs_dir(), f"{name}.pdf") #idk jaky extension chceme
-    #store
-    pass
-
 # A1 - distribution of infected people by age in regions
 def visualizeA1(name, csv_path):
     print("- " + name)
@@ -22,26 +14,28 @@ def visualizeA1(name, csv_path):
     plt.figure()
     sns.set_theme(style="darkgrid")
     ax = sns.boxplot(x="vek", y="kraj", data=df)
-    ax.tick_params(axis='y', rotation=10)
     ax.set(xlabel='Věk', ylabel='Kraj')
     fig = ax.get_figure()
     fig.tight_layout()
     fig.savefig(path.join(utils.graphs_dir(), name+".png"))
 
 # A2 - Series of barplots
-def visualizeA2(name, csv_path):
+def visualizeA2(name, csv1_path, csv2_path):
     print("- " + name)
-    df = pd.read_csv(csv_path)
+    df_people = pd.read_csv(csv1_path)
+    df_regions = pd.read_csv(csv2_path)
+
+    df = df_people.join(df_regions.set_index('kraj_nuts_kod'), on='kraj_nuts_kod')
 
     # Graph 1: total number of vaccination for each region
     sns.set_theme(style="darkgrid")
 
-    df1 = df.groupby(['kraj_nuts_kod']).count()
+    df1 = df.groupby(['kraj_nazev']).count()
     df1 = df1.reset_index()
 
     plt.figure()
-    ax = sns.barplot(x="vekova_skupina", y="kraj_nuts_kod", data=df1)
-    ax.set(xlabel='Počet naočkovaných osob', ylabel='NUTS kód kraje')
+    ax = sns.barplot(x="vekova_skupina", y="kraj_nazev", data=df1)
+    ax.set(xlabel='Počet naočkovaných osob', ylabel='Kraj')
     ax.ticklabel_format(style='plain', axis='x')
     fig = ax.get_figure()
     fig.tight_layout()
@@ -49,13 +43,14 @@ def visualizeA2(name, csv_path):
 
     # Graph 2: total number of vaccination for each region divided by sex
     df2 = df.dropna(subset=['pohlavi']) 
-    df2 = df2.groupby(['kraj_nuts_kod', 'pohlavi']).count()
+    df2 = df2.groupby(['kraj_nazev', 'pohlavi']).count()
     df2 = df2.reset_index()
 
     plt.figure()
-    ax = sns.barplot(x="vekova_skupina", y="kraj_nuts_kod", hue='pohlavi', data=df2)
-    ax.set(xlabel='Počet naočkovaných osob', ylabel='NUTS kód kraje')
+    ax = sns.barplot(x="vekova_skupina", y="kraj_nazev", hue='pohlavi', data=df2)
+    ax.set(xlabel='Počet naočkovaných osob', ylabel='Kraj')
     ax.ticklabel_format(style='plain', axis='x')
+    ax.tick_params(axis='x', rotation=45)
     
     legend = ax.get_legend()
     legend.set_title('Pohlaví')
@@ -71,7 +66,7 @@ def visualizeA2(name, csv_path):
     df3 = df.dropna(subset=['vekova_skupina'])
     # Add value to rows with nan so every row is counted
     df3 = df3.fillna('M')
-    df3 = df3.groupby(['kraj_nuts_kod', 'vekova_skupina']).count()
+    df3 = df3.groupby(['kraj_nazev', 'vekova_skupina']).count()
     df3 = df3.reset_index()
 
     # Create list for groups
@@ -89,12 +84,12 @@ def visualizeA2(name, csv_path):
                 groups_list.append(3)
 
     df3['skupina'] = groups_list
-    df3 = df3.groupby(['kraj_nuts_kod', 'skupina']).sum()
+    df3 = df3.groupby(['kraj_nazev', 'skupina']).sum()
     df3 = df3.reset_index()
 
     plt.figure()
-    ax = sns.barplot(x="pohlavi", y="kraj_nuts_kod", hue='skupina', data=df3)
-    ax.set(xlabel='Počet naočkovaných osob', ylabel='NUTS kód kraje')
+    ax = sns.barplot(x="pohlavi", y="kraj_nazev", hue='skupina', data=df3)
+    ax.set(xlabel='Počet naočkovaných osob', ylabel='Kraj')
     ax.ticklabel_format(style='plain', axis='x')
     ax.tick_params(axis='x', rotation=45)
     
@@ -208,7 +203,103 @@ def visualizeB(name, csv1_path, csv2_path):
 
     #TODO Graph 4: Vaccinated persons for South-Moravian region
 
-# C - 
+# D1 - Show number of vaccinations with each vaccine for all age groups 
+def visualizeD1(name, csv_path):
+    print("- " + name)
+    df = pd.read_csv(csv_path)
+
+    # Count for each class
+    df['count'] = 'ok'
+    df = df.groupby(by=['vekova_skupina', 'vakcina']).count()
+    df = df.reset_index()
+
+    # Make COVID-19 Vaccine Janssen name smaller
+    df['vakcina'] = ['Janssen' if row['vakcina'] == 'COVID-19 Vaccine Janssen' else row['vakcina'] for index, row in df.iterrows()]
+
+    # Graph
+    plt.figure()
+    ax = sns.barplot(x="vekova_skupina", y="count", hue='vakcina', data=df)
+    ax.set(xlabel='Věková skupina', ylabel='Počet očkování')
+    ax.tick_params(axis='x', rotation=45)
+    
+    legend = ax.get_legend()
+    legend.set_title('Vakcína')
+
+    fig = ax.get_figure()
+    fig.tight_layout()
+    fig.savefig(path.join(utils.graphs_dir(), name+".png"))
+
+# D2 - Show vaccination influance on new cases in Prague
+def visualizeD2(name, csv1_path, csv2_path):
+    print("- " + name)
+    df_cases = pd.read_csv(csv1_path)
+    df_vaccination = pd.read_csv(csv2_path)
+    df_vaccination = df_vaccination.sort_values(by=['_id'])
+    df_vaccination = df_vaccination.rename(columns={"_id": "datum", "count": "count"})
+
+    # Cut dataframes to match dates
+    first_vaccination = df_vaccination['datum'].iloc[0]
+    first_case = df_cases['datum'].iloc[0]
+    last_vaccination = df_vaccination['datum'].iloc[-1]
+    last_case = df_cases['datum'].iloc[-1]
+
+    if first_vaccination < first_case:
+        df_vaccination = df_vaccination.drop(df_vaccination[df_vaccination.datum < first_case].index)
+    else:
+        df_cases = df_cases.drop(df_cases[df_cases.datum < first_vaccination].index)
+
+    if last_vaccination > last_case:
+        df_vaccination = df_vaccination.drop(df_vaccination[df_vaccination.datum > last_case].index)
+    else:
+        df_cases = df_cases.drop(df_cases[df_cases.datum > last_vaccination].index)
+
+    # Create cumulative counts
+    df_cases = df_cases.sort_values(by=['datum'])
+    df_vaccination = df_vaccination.sort_values(by=['datum'])
+    df_cases['kumulativni_pocet'] = df_cases['count'].iloc[0]
+    df_vaccination['kumulativni_pocet'] = df_vaccination['count'].iloc[0]
+
+    cumulative_count = list()
+    cases_sum = 0
+    for index, row in df_cases.iterrows():
+        cases_sum += row['count']
+        cumulative_count.append(cases_sum)
+
+    df_cases['kumulativni_pocet'] = cumulative_count
+
+    cumulative_count = list()
+    cases_sum = 0
+    for index, row in df_vaccination.iterrows():
+        cases_sum += row['count']
+        cumulative_count.append(cases_sum)
+
+    df_vaccination['kumulativni_pocet'] = cumulative_count
+
+    # Group data to months
+    df_cases = df_cases.loc[df_cases.datum.str.contains('^\d{4}-\d{2}-01'), :]
+    df_vaccination = df_vaccination.loc[df_vaccination.datum.str.contains('^\d{4}-\d{2}-01'), :]
+
+    # Add column for hue
+    df_cases['typ'] = 'Nakažení'
+    df_vaccination['typ'] = 'Vakcinace'
+
+    df = df_cases.append(df_vaccination, ignore_index=True)
+    df = df.sort_values(by=['datum'])
+
+    # Graph
+    plt.figure()
+    ax = sns.lineplot(x="datum", y="kumulativni_pocet", hue='typ', data=df)
+    ax.set(xlabel='Období', ylabel='Kumulativní pocet výskytů')
+    ax.tick_params(axis='x', rotation=45)
+    
+    legend = ax.get_legend()
+    legend.set_title('Typ')
+
+    fig = ax.get_figure()
+    fig.tight_layout()
+    fig.savefig(path.join(utils.graphs_dir(), name+".png"))
+
+# C - Data preparation for mining 
 def prepareC(name, csv1_path, csv2_path, csv3_path, csv4_path):
     print("- " + name)
     df_cases = pd.read_csv(csv1_path)
@@ -268,13 +359,14 @@ def prepareC(name, csv1_path, csv2_path, csv3_path, csv4_path):
 
 
 ########################################################
-#sns.set(rc = {'figure.figsize':(8,5)})
 #main body
 utils.delete_dir_content(utils.graphs_dir())
 
 visualizeA1("visualizeA1", path.join(utils.extracted_data_dir(), "selectA1.csv"))
-visualizeA2("visualizeA2", path.join(utils.extracted_data_dir(), "selectA2.csv"))
+visualizeA2("visualizeA2", path.join(utils.extracted_data_dir(), "selectA2.csv"), path.join(utils.static_data_dir(), "cz_regions.csv"))
 visualizeB("visualizeB", path.join(utils.extracted_data_dir(), "selectB.csv"), path.join(utils.extracted_data_dir(), "selectB_regions.csv"))
+visualizeD1("visualizeD1", path.join(utils.extracted_data_dir(), "selectD1.csv"))
+visualizeD2("visualizeD2", path.join(utils.extracted_data_dir(), "selectD2_new_cases.csv"), path.join(utils.extracted_data_dir(), "selectD2_vaccinated.csv"))
 prepareC("prepareC", path.join(utils.extracted_data_dir(), "selectC_new_cases.csv"), path.join(utils.extracted_data_dir(), "selectC_vaccinated.csv"), path.join(utils.extracted_data_dir(), "selectC_population.csv"), path.join(utils.static_data_dir(), "top_50_cities.csv"))
 
-print("Done")
+print("Visualization Done")
